861c4eb8ae625cf3aec930eff2c8b612404ce6b2
Syayyiqul Hanif <syayyiqulhanif@gmail.com>
2026-06-05 20:34:27 +0700

    Update report.routes.js

--- backend/src/routes/report.routes.js
+++ backend/src/routes/report.routes.js
@@ -2,14 +2,13 @@ import { Router } from 'express';
 import {
   createReport, getReports, getReportById, updateReport, deleteReport
 } from '../controllers/report.controller.js';
-import { authenticate } from '../middleware/auth.middleware.js';
 
 const router = Router();
 
 router.get('/', getReports);
 router.get('/:id', getReportById);
-router.post('/', authenticate, createReport);
-router.put('/:id', authenticate, updateReport);
-router.delete('/:id', authenticate, deleteReport);
+router.post('/', createReport);
+router.put('/:id', updateReport);
+router.delete('/:id', deleteReport);
 
 export default router;
