8bf3c5a8ce2eaacf63c3e4e481986c2a7408b52f
Firdaus <firdausbudiman1101@gmail.com>
2026-06-06 21:41:34 +0700

    fix routing

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,17 +1,26 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
     root: '.',
     server: {
         port: 5173,
         proxy: {
-        '/api': {
-            target: 'http://localhost:3000',
-            changeOrigin: true,
-        },
+            '/api': {
+                target: 'http://localhost:3000',
+                changeOrigin: true,
+            },
         },
     },
     build: {
         outDir: 'dist',
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+                prediction: resolve(__dirname, 'src/pages/prediction.html'),
+                map: resolve(__dirname, 'src/pages/map.html'),
+                reports: resolve(__dirname, 'src/pages/reports.html'),
+            }
+        }
     },
 });
